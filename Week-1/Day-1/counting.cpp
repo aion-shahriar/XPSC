a01dc1a83a94baa3b055f495aff0a15a885376b5
aion-shahriar <163476501+aion-shahriar@users.noreply.github.com>
2024-03-18 20:04:33 +0600

    Create counting.cpp
    atcoder

---
+++ Week-1/Day-1/counting.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  int a,b;
+  cin>>a>>b;
+  if(a>=b) cout<<0<<"\n";
+  else cout<<b-a+1<<"\n";
+  
+  return 0;
+}
