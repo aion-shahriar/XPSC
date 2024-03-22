0cdacf7b19f9800deddce7415f6849ea0b8f4e8b
aion-shahriar <163476501+aion-shahriar@users.noreply.github.com>
2024-03-22 15:14:49 +0600

    Create Plus_or_minus.cpp
    codeforces

---
+++ Week-2/Day-1/Plus_or_minus.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a + b == c)
+        {
+            cout << "+"
+                 << "\n";
+        }
+        else
+        {
+            cout << "-"
+                 << "\n";
+        }
+    }
+    return 0;
+}
