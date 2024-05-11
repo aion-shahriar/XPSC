c4320cbc5a4cb285221d437e086d232f958b3aa6
aion-shahriar <163476501+aion-shahriar@users.noreply.github.com>
2024-05-11 19:34:20 +0600

    Create K-th_divisor.cpp
    codeforces

---
+++ Week-8/Day-1/K-th_divisor.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long n,k;
+    cin>>n>>k;
+
+    vector<long long>divisors;
+
+    for(long long i=1;i*i<=n;i++) {
+        if(n%i==0) {
+            divisors.push_back(i);
+            if((n/i)!=i) {
+                divisors.push_back(n/i);
+            }
+        }
+    }
+    sort(divisors.begin(),divisors.end());
+    if(divisors.size()<k) {
+        cout<<-1<<"\n";
+    }
+    else {
+        cout<<divisors[k-1]<<"\n";
+    }
+
+    // for(int i=0;i<divisors.size();i++) {
+    //     cout<<divisors[i]<<" ";
+    // }
+
+    
+    return 0;
+}
